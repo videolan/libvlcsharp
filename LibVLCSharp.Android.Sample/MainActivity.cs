612f393340556c56aefe6d98d0354cb9d18caaef
Martin Finkel <martin@videolabs.io>
2018-01-07 14:56:47 +0100

    WIP integrating libvlc with SurfaceView

---
+++ LibVLCSharp.Android.Sample/MainActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace LibVLCSharp.Android.Sample
+{
+    [Activity(Label = "LibVLCSharp.Android.Sample", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
