e21df256d11be0eed1996ebef5e14decdd968f86
Martin Finkel <martin@videolabs.io>
2018-02-09 19:42:50 +0100

    libvlc_renderer_item_hold call is currently failing

--- LibVLCSharp.Tests/RendererDiscovererTests.cs
+++ LibVLCSharp.Tests/RendererDiscovererTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using NUnit.Framework;
 using VideoLAN.LibVLC;
 using static System.Diagnostics.Debug;
@@ -9,12 +10,13 @@ namespace LibVLCSharp.Tests
     public class RendererDiscovererTests
     {
         [Test]
-        public async Task T()
+        public async Task DiscoverItems()
         {
             var instance = new Instance();
 
             var rendererDiscoverer = new RendererDiscoverer(instance, "microdns");
             var em = rendererDiscoverer.EventManager;
+            var itemAdded = false;
 
             em.ItemAdded += (sender, args) =>
             {
@@ -23,12 +25,22 @@ namespace LibVLCSharp.Tests
                     WriteLine("Can render video");
                 if(args.RendererItem.CanRenderAudio)
                     WriteLine("Can render audio");
+                itemAdded = true;
             };
 
             if(!rendererDiscoverer.Start())
                 NUnit.Framework.Assert.Fail();
 
-            await Task.Delay(1000);
+            await Task.Delay(10000);
+
+            NUnit.Framework.Assert.True(itemAdded);
+        }
+
+        [Test]
+        public void RetrieveListInformation()
+        {
+            var rd = new RendererDiscoverer(new Instance(), "rd");
+            NUnit.Framework.Assert.Positive(rd.List.Length);
         }
     }
 }
