e0c8001dc93b90da2483f42d3ae60d4b91982e9a
Ch4rg3r <ch4rg3r@hotmail.de>
2018-07-03 18:52:07 +0200

    Basic Wpf UserControl
    Added Wpf UserControl with Autosizing using WindowsFormsHost

---
+++ LibVLCSharp.Wpf/VLCViewControl.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LibVLCSharp.Wpf
+{
+    public partial class VLCViewControl : UserControl, LibVLCSharp.Shared.IVideoView
+    {
+        private LibVLCSharp.Shared.MediaPlayer mediaPlayer;
+        private LibVLCSharp.Shared.LibVLC libVLC;
+        private double controlWidth;
+        private double controlHeight;
+
+        public VLCViewControl()
+        {
+            Shared.Core.Initialize();
+            libVLC = new Shared.LibVLC();
+            mediaPlayer = new Shared.MediaPlayer(libVLC);
+
+            InitializeComponent();
+            this.SizeChanged += OnSizeChanged;
+
+            controlHeight = this.Height;
+            controlWidth = this.Width;
+
+            mediaPlayer.Hwnd = PlayerView.Handle;
+        }
+
+        private void VLCResize()
+        {
+            uint h = 0, w = 0;
+            float scale = 1, scalew = 1, scaleh = 1;
+
+            if (mediaPlayer.Size(0, ref w, ref h))
+            {
+                scalew = (float)controlWidth / (float)w;
+                w = (uint)controlWidth;
+
+                scaleh = (float)controlHeight / (float)h;
+                h = (uint)controlHeight;
+
+                scale = scalew < scaleh ? scalew : scaleh;
+                mediaPlayer.Scale = scale;
+            }
+        }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            controlWidth = e.NewSize.Width;
+            controlHeight = e.NewSize.Height;
+
+            if (mediaPlayer.IsPlaying)
+            {
+                VLCResize();
+            }
+        }
+
+        public LibVLCSharp.Shared.MediaPlayer MediaPlayer { get => mediaPlayer; }
+        public LibVLCSharp.Shared.LibVLC LibVLC { get => libVLC; }
+    }
+}
---
+++ LibVlcSharp.Wpf.Sample/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibVLCSharp.Shared;
+
+namespace LibVLCSharp.Wpf.Sample
+{
+    public partial class MainWindow : Window
+    {
+        private System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Player.MediaPlayer.Play(new Media(Player.LibVLC,
+                "http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4", Media.FromType.FromLocation));
+
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 10);
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            this.Width = 900;
+            this.Height = 750;
+            dispatcherTimer.Stop();
+        }
+    }
+}
