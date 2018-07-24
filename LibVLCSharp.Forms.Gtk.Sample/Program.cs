ad2811a96c2f4e0014453658b6374c8bc35e7b1a
Jérémy VIGNELLES <jeremy.vignelles@gmail.com>
2018-07-24 19:03:31 +0200

    WIP : Implementing GTK# Xamarin.Forms
    Restored commit lost after rebase

---
+++ LibVLCSharp.Forms.Gtk.Sample/Program.cs
@@ -0,0 +1,28 @@
+﻿using LibVLCSharp.Forms.Sample;
+using LibVLCSharp.Forms.Shared;
+using System;
+using Xamarin.Forms.Platform.GTK;
+
+namespace LibVLCSharp.Forms.Gtk.Sample
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            global::Gtk.Application.Init();
+            LibVLCSharpFormsRenderer.Init();
+
+            // For some reason, Xamarin does not pick the LibVLCSharp.Form.Platforms.Gtk assembly as a renderer assembly. Add it manually.
+            global::Xamarin.Forms.Forms.Init(new [] { typeof(LibVLCSharp.Forms.Platforms.Gtk.VideoViewRenderer).Assembly });
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("GTK# LibVLCSharp sample");
+            window.Show();
+
+            global::Gtk.Application.Run();
+        }
+    }
+}
---
+++ LibVLCSharp.Forms.Platforms.Gtk/VideoViewRenderer.cs
@@ -0,0 +1,43 @@
+﻿using LibVLCSharp.Forms.Platforms.Gtk;
+using LibVLCSharp.Forms.Shared;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+[assembly: ExportRenderer(typeof(LibVLCSharp.Forms.Shared.VideoView), typeof(VideoViewRenderer))]
+namespace LibVLCSharp.Forms.Platforms.Gtk
+{
+    public class VideoViewRenderer : ViewRenderer<LibVLCSharp.Forms.Shared.VideoView, LibVLCSharp.Platforms.Gtk.VideoView>
+    {
+        LibVLCSharp.Platforms.Gtk.VideoView _videoView;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<VideoView> e)
+        {
+            base.OnElementChanged(e);
+
+            if(Control == null)
+            {
+                _videoView = new LibVLCSharp.Platforms.Gtk.VideoView(Element.CliOptions);
+                SetNativeControl(_videoView);
+                
+                Element.LibVLC = Control.LibVLC;
+                Element.MediaPlayer = Control.MediaPlayer;
+            }
+
+            if (e.OldElement != null)
+            {
+            }
+
+            if (e.NewElement != null)
+            {
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            _videoView.Dispose();
+        }
+    }
+}
