d5ccfe5ec9dadb2dd3f674ace94619ba9bf76165
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-07-28 12:30:13 +0200

    Added support to Xamarin.Forms WPF Backend

---
+++ LibVLCSharp.Forms.Platforms.WPF/VideoViewRenderer.cs
@@ -0,0 +1,43 @@
+﻿using LibVLCSharp.Forms.Platforms.WPF;
+using LibVLCSharp.Forms.Shared;
+using Xamarin.Forms.Platform.WPF;
+
+[assembly: ExportRenderer(typeof(VideoView), typeof(VideoViewRenderer))]
+namespace LibVLCSharp.Forms.Platforms.WPF
+{
+    public class VideoViewRenderer : ViewRenderer<VideoView, LibVLCSharp.WPF.VideoView>
+    {
+        LibVLCSharp.WPF.VideoView _videoView;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<VideoView> e)
+        {
+            if (Control == null)
+            {
+                _videoView = new LibVLCSharp.WPF.VideoView();
+                SetNativeControl(_videoView);
+            }
+
+            if (e.OldElement != null)
+            {
+            }
+
+            if (e.NewElement != null)
+            {
+                UpdateMediaPlayer();
+                UpdateLibVLC();
+            }
+
+            base.OnElementChanged(e);
+        }
+
+        void UpdateMediaPlayer()
+        {
+            Element.MediaPlayer = Control.MediaPlayer;
+        }
+
+        void UpdateLibVLC()
+        {
+            Element.LibVLC = Control.LibVLC;
+        }
+    }
+}
---
+++ LibVLCSharp.Forms.WPF.Sample/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using LibVLCSharp.Forms.Platforms.WPF;
+using LibVLCSharp.Forms.Shared;
+using System.Collections.Generic;
+using System.Reflection;
+using Xamarin.Forms.Platform.WPF;
+
+namespace LibVLCSharp.Forms.WPF.Sample
+{
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var test = new List<Assembly>
+            {
+                typeof(VideoView).Assembly,
+                typeof(VideoViewRenderer).Assembly
+            };
+
+            Xamarin.Forms.Forms.Init();
+
+            LoadApplication(new Forms.Sample.App());
+        }
+    }
+}
