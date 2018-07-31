c5dd20c58ef89881e6ae1f0b84c2ea2007f21200
Stéphane Mitermite <stephthebest@gmail.com>
2018-08-01 00:51:44 +0200

    Simplifies and improves the WPF VideoView (removes the OnContentChanged method) Adds an example for styling

--- LibVLCSharp.WPF.Sample/Example2.xaml.cs
+++ LibVLCSharp.WPF.Sample/Example2.xaml.cs
@@ -1,5 +1,4 @@
-﻿using LibVLCSharp.Shared;
-
+﻿
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -12,31 +11,13 @@ namespace LibVLCSharp.WPF.Sample
         {
             InitializeComponent();
 
-            var label = new Label
+            test.Children.Add(new Label
             {
                 Content = "TEST",
                 HorizontalAlignment = HorizontalAlignment.Right,
                 VerticalAlignment = VerticalAlignment.Bottom,
                 Foreground = new SolidColorBrush(Colors.Red)
-            };
-            test.Children.Add(label);
-        }
-
-        void StopButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (Player.MediaPlayer.IsPlaying)
-            {
-                Player.MediaPlayer.Stop();
-            }
-        }
-
-        void PlayButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (!Player.MediaPlayer.IsPlaying)
-            {
-                Player.MediaPlayer.Play(new Media(Player.LibVLC,
-                    "http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4", Media.FromType.FromLocation));
-            }
+            });
         }
     }
 }
---
+++ LibVLCSharp.WPF.Sample/Example3.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace LibVLCSharp.WPF.Sample
+{
+    /// <summary>
+    /// Logique d'interaction pour Example3.xaml
+    /// </summary>
+    public partial class Example3 : Window
+    {
+        public Example3()
+        {
+            InitializeComponent();
+        }
+    }
+}
