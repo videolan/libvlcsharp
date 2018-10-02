951bbc91c326ddefe46478254844677b972ed728
Martin Finkel <martin@videolabs.io>
2018-10-02 11:01:47 +0700

    Cleanup solution structure
    - Move all samples to Samples folder
    - Put Forms samples in Samples/Forms
    - Rename LibVLSharp.Forms.WPF.Sample to LibVLCSharp.Forms.Sample.WPF
    - Fix build order for Android: LibVLCSharp for the Android target depends on LibVLCSharp.Android.AWindow being built first. This is now done automatically.

--- Samples/Forms/LibVLCSharp.Forms.WPF.Sample/Properties/AssemblyInfo.cs
+++ Samples/Forms/LibVLCSharp.Forms.WPF.Sample/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LibVLCSharp.Forms.WPF.Sample")]
+[assembly: AssemblyTitle("LibVLCSharp.Forms.Sample.WPF")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LibVLCSharp.Forms.WPF.Sample")]
+[assembly: AssemblyProduct("LibVLCSharp.Forms.Sample.WPF")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Samples/Sample/Program.cs
+++ Samples/Sample/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using LibVLCSharp;
 using LibVLCSharp.Shared;
 
 namespace Sample
