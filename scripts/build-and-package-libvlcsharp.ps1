2180533d26b27aa236fff0b2a9a6800df457b874
Martin Finkel <martin@videolabs.io>
2018-10-04 16:39:50 +0700

    Add netstandard1.1 and net40 targets to nuspec

--- scripts/build-and-package-libvlcsharp.ps1
+++ scripts/build-and-package-libvlcsharp.ps1
@@ -1,3 +1,3 @@
 $msbuild =  '"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"'
-iex ("& {0} {1}" -f $msbuild, "../LibVLCSharp/LibVLCSharp.csproj /t:Build /p:Configuration=Release")
+iex ("& {0} {1}" -f $msbuild, "../LibVLCSharp/LibVLCSharp.csproj /t:Restore,Clean,Build /p:Configuration=Release")
 ..\nuget.exe pack ..\LibVLCSharp.nuspec
