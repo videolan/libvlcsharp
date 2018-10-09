c44644c66462d6aaf3d8d989422ffa843a6eead5
Martin Finkel <martin@videolabs.io>
2018-10-09 09:59:29 +0700

    Add Windows net40 sample

---
+++ LibVLCSharp.Windows.Net40.Sample/Program.cs
@@ -0,0 +1,19 @@
+﻿using LibVLCSharp.Shared;
+using System;
+
+namespace LibVLCSharp.Windows.Net40.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Core.Initialize();
+
+            var libVLC = new LibVLC();
+            var media = new Media(libVLC, "http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4", Media.FromType.FromLocation);
+            var mp = new MediaPlayer(media);
+            mp.Play();
+            Console.ReadKey();
+        }
+    }
+}
