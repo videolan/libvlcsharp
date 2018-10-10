0a47f40e4bba8ced01f1b7d32f14d1e6a1c514a6
Martin Finkel <martin@videolabs.io>
2018-10-10 10:39:17 +0200

    add packaging for LibVLCSharp.Forms

---
+++ scripts/build-and-package-libvlcsharp-forms.ps1
@@ -0,0 +1,3 @@
+$msbuild =  '"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"'
+iex ("& {0} {1}" -f $msbuild, "../LibVLCSharp.Forms/LibVLCSharp.Forms.csproj /t:Restore,Clean,Build /p:Configuration=Release")
+..\nuget.exe pack ..\LibVLCSharp.Forms.nuspec
