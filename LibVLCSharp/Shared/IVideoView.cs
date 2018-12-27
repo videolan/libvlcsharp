5a3456583bc5ddcdecd5baaff3733d58fdf3b073
Martin Finkel <martin@videolabs.io>
2018-12-27 12:06:15 +0700

    [Docs] IVideoView
    (cherry picked from commit 9c258b5b5718c18f87b9951effc996dc310c2be5)

--- LibVLCSharp/Shared/IVideoView.cs
+++ LibVLCSharp/Shared/IVideoView.cs
@@ -1,9 +1,13 @@
-﻿using System;
-
-namespace LibVLCSharp.Shared
+﻿namespace LibVLCSharp.Shared
 {
+    /// <summary>
+    /// VideoView Interface
+    /// </summary>
     public interface IVideoView
     {
+        /// <summary>
+        /// MediaPlayer object connected to the view
+        /// </summary>
         MediaPlayer MediaPlayer { get; }
     }
 }
