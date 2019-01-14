fa455586d1b93ee903fd1e34d49b59b323615754
Martin Finkel <martin@videolabs.io>
2019-01-14 12:01:15 +0700

    Enable Xamarin.Forms crossplatform animations for iOS by implementing IVisualElementRenderer
    This is hopefully a temporary workaround
    
    https://github.com/xamarin/Xamarin.Forms/issues/4885
    (cherry picked from commit 02c986f07a09a4b257759edc1a7782df8bf70a78)

--- LibVLCSharp.Forms/Platforms/iOS/VideoViewRenderer.cs
+++ LibVLCSharp.Forms/Platforms/iOS/VideoViewRenderer.cs
@@ -5,19 +5,21 @@ using LibVLCSharp.Forms.Shared;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
+using UIKit;
+using System;
 
-[assembly: ExportRenderer(typeof(VideoView), typeof(VideoViewRenderer))]
+[assembly: ExportRenderer(typeof(LibVLCSharp.Forms.Shared.VideoView), typeof(VideoViewRenderer))]
 namespace LibVLCSharp.Forms.Platforms.iOS
 {
     public class VideoViewRenderer : ViewRenderer<LibVLCSharp.Forms.Shared.VideoView, LibVLCSharp.Platforms.iOS.VideoView>
     {
-        protected override void OnElementChanged(ElementChangedEventArgs<VideoView> e)
+        protected override void OnElementChanged(ElementChangedEventArgs<LibVLCSharp.Forms.Shared.VideoView> e)
         {
             base.OnElementChanged(e);
 
             if(Control == null)
             {
-                SetNativeControl(new LibVLCSharp.Platforms.iOS.VideoView());
+                SetNativeControl(new LibVLCSharp.Forms.Platforms.iOS.VideoView());
             }
 
             if (e.OldElement != null)
@@ -35,5 +37,27 @@ namespace LibVLCSharp.Forms.Platforms.iOS
         {
             Control.MediaPlayer = e.NewMediaPlayer;
         }
+    }
+
+    public class VideoView : LibVLCSharp.Platforms.iOS.VideoView, IVisualElementRenderer
+    {
+        public VisualElement Element { get; private set; }
+
+        public UIView NativeView => this;
+
+        public UIViewController ViewController => ViewController;
+
+        public event EventHandler<VisualElementChangedEventArgs> ElementChanged;
+
+        public SizeRequest GetDesiredSize(double widthConstraint, double heightConstraint) => this.GetSizeRequest(widthConstraint, heightConstraint);
+
+        public void SetElement(VisualElement element)
+        {
+            Element = element;
+
+            ElementChanged?.Invoke(this, new VisualElementChangedEventArgs(null, Element));
+        }
+
+        public void SetElementSize(Size size) => Element.Layout(new Rectangle(Element.X, Element.Y, size.Width, size.Height));
     }
 }
