5e8d54a765ab702db972f5ee66ca5078b426a048
Martin Finkel <martin@videolabs.io>
2019-01-15 11:58:00 +0700

    remove nuget pack from ps script since its done in the csproj now

--- scripts/build-and-package-libvlcsharp-wpf.ps1
+++ scripts/build-and-package-libvlcsharp-wpf.ps1
@@ -3,4 +3,3 @@ Remove-Item ..\LibVLCSharp.WPF\obj -Force -Recurse
 
 $msbuild =  '"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"'
 iex ("& {0} {1}" -f $msbuild, "../LibVLCSharp.WPF/LibVLCSharp.WPF.csproj /t:Restore,Clean,Build /p:Configuration=Release")
-..\nuget.exe pack ..\LibVLCSharp.WPF.nuspec
