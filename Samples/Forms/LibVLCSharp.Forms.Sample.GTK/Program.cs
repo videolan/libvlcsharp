ffce09649ba3b3c469b11f3e4e24b8433c9f8762
Martin Finkel <martin@videolabs.io>
2019-01-19 21:00:16 +0700

    nitpick: GTK.Forms sample

--- Samples/Forms/LibVLCSharp.Forms.Sample.GTK/Program.cs
+++ Samples/Forms/LibVLCSharp.Forms.Sample.GTK/Program.cs
@@ -13,13 +13,14 @@ namespace LibVLCSharp.Forms.Sample.GTK
             global::Gtk.Application.Init();
             LibVLCSharpFormsRenderer.Init();
 
-            // For some reason, Xamarin does not pick the LibVLCSharp.Form.Platforms.Gtk assembly as a renderer assembly. Add it manually.
+            // For some reason, Xamarin does not pick the LibVLCSharp.Form.Platforms.Gtk assembly as a renderer assembly. 
+            // Add it manually.
             global::Xamarin.Forms.Forms.Init(new[] { typeof(LibVLCSharp.Forms.Platforms.GTK.VideoViewRenderer).Assembly });
 
             var app = new App();
             var window = new FormsWindow();
             window.LoadApplication(app);
-            window.SetApplicationTitle("GTK# LibVLCSharp sample");
+            window.SetApplicationTitle("GTK# LibVLCSharp.Forms sample");
             window.Show();
 
             global::Gtk.Application.Run();
