ccb2d5cdb114c266a4f5edffa0586284cb5cb9b8
Stéphane Mitermite <stephthebest@gmail.com>
2019-03-09 15:11:30 +0100

    Renames BindingContext property to ViewModel

--- Samples/Forms/LibVLCSharp.Forms.Sample/MainPage.xaml.cs
+++ Samples/Forms/LibVLCSharp.Forms.Sample/MainPage.xaml.cs
@@ -9,22 +9,22 @@ namespace LibVLCSharp.Forms.Sample
         {
             InitializeComponent();
             videoView.MediaPlayerChanged += VideoView_MediaPlayerChanged;
-            BindingContext.PropertyChanged += BindingContext_PropertyChanged;
-            BindingContext.Play();
+            ViewModel.PropertyChanged += BindingContext_PropertyChanged;
+            ViewModel.Play();
         }
 
-        private new MainViewModel BindingContext => (MainViewModel)base.BindingContext;
+        private MainViewModel ViewModel => (MainViewModel)BindingContext;
 
         private void BindingContext_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
-            if (e.PropertyName.Equals(nameof(BindingContext.MediaPlayer)))
+            if (e.PropertyName.Equals(nameof(ViewModel.MediaPlayer)))
                 Debug.WriteLine("MediaPlayer change raised from ViewModel.Propertychanged");
         }
 
         private void VideoView_MediaPlayerChanged(object sender, LibVLCSharp.Shared.MediaPlayerChangedEventArgs e)
         {
             Debug.WriteLine("VideoView_MediaPlayerChanged");
-            BindingContext.Play();
+            ViewModel.Play();
         }
     }
 }
