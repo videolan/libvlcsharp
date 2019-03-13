41661f81f190d198c4f7edf935ad43eaedff5829
Stéphane Mitermite <stephthebest@gmail.com>
2019-03-13 14:02:20 +0100

    Add project files.

---
+++ Sample/Sample/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Sample
+{
+    /// <summary>
+    /// Represents the sample application.
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="App"/> class.
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
