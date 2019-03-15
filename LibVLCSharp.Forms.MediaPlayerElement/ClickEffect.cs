43ab6039c07a07f5f5407458b3fed8d4af247cde
Stéphane Mitermite <stephthebest@gmail.com>
2019-03-15 10:49:47 +0100

    Adds zoom button and clickeffect

---
+++ LibVLCSharp.Forms.MediaPlayerElement/ClickEffect.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace LibVLCSharp.Forms
+{
+    /// <summary>
+    /// Click effect.
+    /// </summary>
+    public class ClickEffect : TriggerAction<VisualElement>
+    {
+        /// <summary>
+        /// Apply a click effect.
+        /// </summary>
+        /// <param name="sender">The object on which to invoke the trigger action.</param>
+        protected override async void Invoke(VisualElement sender)
+        {
+            await sender.ScaleTo(0.85, 100);
+            await sender.ScaleTo(1, 50);
+        }
+    }
+}
