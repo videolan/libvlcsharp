9047c5e49a967b2250fda8d5be13ecee4668deb9
Stéphane Mitermite <stephthebest@gmail.com>
2019-03-18 15:37:27 +0100

    Reorder the methods

--- LibVLCSharp.Forms.MediaPlayerElement/MediaPlayerElement.xaml.cs
+++ LibVLCSharp.Forms.MediaPlayerElement/MediaPlayerElement.xaml.cs
@@ -22,28 +22,6 @@ namespace LibVLCSharp.Forms
             InitializeComponent();
         }
 
-        private static readonly BindableProperty VideoViewProperty = BindableProperty.Create(nameof(VideoView), typeof(VideoView),
-            typeof(MediaPlayerElement), propertyChanged: VideoViewPropertyChanged);
-        /// <summary>
-        /// Gets or sets the video view.
-        /// </summary>
-        public VideoView VideoView
-        {
-            get => (VideoView)GetValue(VideoViewProperty);
-            set => SetValue(VideoViewProperty, value);
-        }
-
-        private static readonly BindableProperty PlaybackControlsProperty = BindableProperty.Create(nameof(PlaybackControls), typeof(PlaybackControls),
-             typeof(MediaPlayerElement), propertyChanged: PlaybackControlsPropertyChanged);
-        /// <summary>
-        /// Gets or sets the playback controls for the media.
-        /// </summary>
-        public PlaybackControls PlaybackControls
-        {
-            get => (PlaybackControls)GetValue(PlaybackControlsProperty);
-            set => SetValue(PlaybackControlsProperty, value);
-        }
-
         /// <summary>
         /// Identifies the <see cref="LibVLC"/> dependency property.
         /// </summary>
@@ -72,19 +50,33 @@ namespace LibVLCSharp.Forms
             set => SetValue(MediaPlayerProperty, value);
         }
 
+        private static readonly BindableProperty PlaybackControlsProperty = BindableProperty.Create(nameof(PlaybackControls), typeof(PlaybackControls),
+            typeof(MediaPlayerElement), propertyChanged: PlaybackControlsPropertyChanged);
+        /// <summary>
+        /// Gets or sets the playback controls for the media.
+        /// </summary>
+        public PlaybackControls PlaybackControls
+        {
+            get => (PlaybackControls)GetValue(PlaybackControlsProperty);
+            set => SetValue(PlaybackControlsProperty, value);
+        }
+
+        private static readonly BindableProperty VideoViewProperty = BindableProperty.Create(nameof(VideoView), typeof(VideoView),
+          typeof(MediaPlayerElement), propertyChanged: VideoViewPropertyChanged);
+        /// <summary>
+        /// Gets or sets the video view.
+        /// </summary>
+        public VideoView VideoView
+        {
+            get => (VideoView)GetValue(VideoViewProperty);
+            set => SetValue(VideoViewProperty, value);
+        }
+
         private void OnVideoViewChanged(VideoView videoView)
         {
             if (videoView != null)
             {
                 videoView.MediaPlayer = MediaPlayer;
-            }
-        }
-
-        private void OnPlayControlsChanged(PlaybackControls playbackControls)
-        {
-            if (playbackControls != null)
-            {
-                playbackControls.MediaPlayer = MediaPlayer;
             }
         }
 
@@ -111,14 +103,17 @@ namespace LibVLCSharp.Forms
             }
         }
 
+        private void OnPlayControlsChanged(PlaybackControls playbackControls)
+        {
+            if (playbackControls != null)
+            {
+                playbackControls.MediaPlayer = MediaPlayer;
+            }
+        }
+
         private static void VideoViewPropertyChanged(BindableObject bindable, object oldValue, object newValue)
         {
             ((MediaPlayerElement)bindable).OnVideoViewChanged((VideoView)newValue);
-        }
-
-        private static void PlaybackControlsPropertyChanged(BindableObject bindable, object oldValue, object newValue)
-        {
-            ((MediaPlayerElement)bindable).OnPlayControlsChanged((PlaybackControls)newValue);
         }
 
         private static void LibVLCPropertyChanged(BindableObject bindable, object oldValue, object newValue)
@@ -129,6 +124,11 @@ namespace LibVLCSharp.Forms
         private static void MediaPlayerPropertyChanged(BindableObject bindable, object oldValue, object newValue)
         {
             ((MediaPlayerElement)bindable).OnMediaPlayerChanged((MediaPlayer)newValue);
+        }
+
+        private static void PlaybackControlsPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            ((MediaPlayerElement)bindable).OnPlayControlsChanged((PlaybackControls)newValue);
         }
 
         /// <summary>
