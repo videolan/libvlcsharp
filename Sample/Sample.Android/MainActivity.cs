1005c1770d911b9ed9969bc30fea6564d1266805
Stéphane Mitermite <stephthebest@gmail.com>
2019-03-22 21:00:21 +0100

    Removes Xamarin.Essentials package

--- Sample/Sample.Android/MainActivity.cs
+++ Sample/Sample.Android/MainActivity.cs
@@ -1,10 +1,8 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
-using Android.Runtime;
 using Android.Views;
 using LibVLCSharp.Forms.Shared;
-using Xamarin.Essentials;
 
 namespace Sample.Droid
 {
@@ -29,25 +27,11 @@ namespace Sample.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-            Platform.Init(this, savedInstanceState);
             LibVLCSharpFormsRenderer.Init();
             Window.DecorView.SystemUiVisibility = Window.DecorView.SystemUiVisibility |
                 (StatusBarVisibility)(SystemUiFlags.ImmersiveSticky | SystemUiFlags.Fullscreen | SystemUiFlags.HideNavigation);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
-        }
-
-        /// <summary>
-        /// Callback for the result from requesting permissions.
-        /// </summary>
-        /// <param name="requestCode">The request code.</param>
-        /// <param name="permissions">The requested permissions. Never null.</param>
-        /// <param name="grantResults">The grant results for the corresponding permissions which is either <see cref="Permission.Granted"/> or
-        /// <see cref="Permission.Denied"/>. Never null.</param>
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum]Permission[] grantResults)
-        {
-            Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
