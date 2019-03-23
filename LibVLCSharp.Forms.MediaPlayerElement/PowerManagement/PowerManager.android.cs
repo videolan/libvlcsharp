9447cac2b1125d735684b5418cdf957622d93eea
Stéphane Mitermite <stephthebest@gmail.com>
2019-03-23 16:49:33 +0100

    Adds KeepScreenOn feature

---
+++ LibVLCSharp.Forms.MediaPlayerElement/PowerManagement/PowerManager.android.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Views;
+
+namespace LibVLCSharp.Forms.PowerManagement
+{
+    /// <summary>
+    /// Power manager.
+    /// </summary>
+    public static class PowerManager
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the screen should be kept on.
+        /// </summary>
+        public static bool KeepScreenOn
+        {
+            get => (Platform.Activity?.Window?.Attributes?.Flags ?? 0).HasFlag(WindowManagerFlags.KeepScreenOn);
+            set
+            {
+                var window = Platform.Activity?.Window;
+                if (window != null)
+                {
+                    if (value)
+                    {
+                        window.AddFlags(WindowManagerFlags.KeepScreenOn);
+                    }
+                    else
+                    {
+                        window.ClearFlags(WindowManagerFlags.KeepScreenOn);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LibVLCSharp.Forms.MediaPlayerElement/PowerManagement/PowerManager.ios.cs
@@ -0,0 +1,19 @@
+﻿using UIKit;
+
+namespace LibVLCSharp.Forms.PowerManagement
+{
+    /// <summary>
+    /// Power manager.
+    /// </summary>
+    public static class PowerManager
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the screen should be kept on.
+        /// </summary>
+        public static bool KeepScreenOn
+        {
+            get => UIApplication.SharedApplication.IdleTimerDisabled;
+            set => UIApplication.SharedApplication.IdleTimerDisabled = value;
+        }
+    }
+}
