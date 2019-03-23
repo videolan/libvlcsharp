abca7a8a9b7acd4ea1f451ac6cfebfb4db354cae
Stéphane Mitermite <stephthebest@gmail.com>
2019-03-23 16:49:33 +0100

    Adds KeepScreenOn feature

---
+++ LibVLCSharp.Forms.MediaPlayerElement/PowerManagement/PowerManager.mac.cs
@@ -0,0 +1,18 @@
+﻿
+namespace LibVLCSharp.Forms.PowerManagement
+{
+    /// <summary>
+    /// Power manager.
+    /// </summary>
+    public static class PowerManager
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the screen should be kept on.
+        /// </summary>
+        public static bool KeepScreenOn
+        {
+            get => false;
+            set { }
+        }
+    }
+}
