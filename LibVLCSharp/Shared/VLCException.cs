3bdb84c676661ef6f5556061d8e05c3282d28b04
Jérémy VIGNELLES <jeremy.vignelles@gmail.com>
2019-04-07 14:38:11 +0200

    Code cleanup + improved errors management

--- LibVLCSharp/Shared/VLCException.cs
+++ LibVLCSharp/Shared/VLCException.cs
@@ -7,5 +7,12 @@ namespace LibVLCSharp.Shared
         public VLCException(string message = ""):base(message)
         {
         }
+
+        /// <summary>
+        /// Creates a <see cref="VLCException" /> with a message and an inner exeption
+        /// </summary>
+        public VLCException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
     }
 }
