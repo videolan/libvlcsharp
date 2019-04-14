08e13122904f238b95b918fc09d737619897e3c9
Stéphane Mitermite <stephthebest@gmail.com>
2019-04-14 14:40:12 +0200

    Adds missing platform classes

---
+++ LibVLCSharp.Forms.MediaPlayerElement/Platforms/iOS/Platform.cs
@@ -0,0 +1,15 @@
+﻿namespace LibVLCSharp.Forms.Platforms.iOS
+{
+    /// <summary>
+    /// Empty shell used to load the custom renderer assembly.
+    /// </summary>
+    public static class Platform
+    {
+        /// <summary>
+        /// Call this to load the custom renderer assembly.
+        /// </summary>
+        public static void Init()
+        {
+        }
+    }
+}
