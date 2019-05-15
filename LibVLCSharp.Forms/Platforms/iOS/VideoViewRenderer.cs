03437bacc55e18a525cdb5efd08c8dbf317e5a6e
Martin Finkel <martin@videolabs.io>
2019-05-15 11:20:52 +0700

    LibVLCSharp.Forms iOS: remove temporary workaround
    https://code.videolan.org/videolan/LibVLCSharp/commit/fa455586d1b93ee903fd1e34d49b59b323615754

--- LibVLCSharp.Forms/Platforms/iOS/VideoViewRenderer.cs
+++ LibVLCSharp.Forms/Platforms/iOS/VideoViewRenderer.cs
@@ -42,26 +42,4 @@ namespace LibVLCSharp.Forms.Platforms.iOS
             Control.MediaPlayer = e.NewMediaPlayer;
         }
     }
-
-    public class VideoView : LibVLCSharp.Platforms.iOS.VideoView, IVisualElementRenderer
-    {
-        public VisualElement Element { get; private set; }
-
-        public UIView NativeView => this;
-
-        public UIViewController ViewController => ViewController;
-
-        public event EventHandler<VisualElementChangedEventArgs> ElementChanged;
-
-        public SizeRequest GetDesiredSize(double widthConstraint, double heightConstraint) => this.GetSizeRequest(widthConstraint, heightConstraint);
-
-        public void SetElement(VisualElement element)
-        {
-            Element = element;
-
-            ElementChanged?.Invoke(this, new VisualElementChangedEventArgs(null, Element));
-        }
-
-        public void SetElementSize(Size size) => Element.Layout(new Rectangle(Element.X, Element.Y, size.Width, size.Height));
-    }
 }
