7447e085e991f0ab1ee2a95c0eb81faa158fde5f
Stéphane Mitermite <stephthebest@gmail.com>
2019-05-19 16:31:29 +0200

    Manages application lifecycle

--- Sample/Sample/App.xaml.cs
+++ Sample/Sample/App.xaml.cs
@@ -3,7 +3,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-namespace Sample
+namespace LibVLCSharp.Forms.Sample.MediaPlayerElement
 {
     /// <summary>
     /// Represents the sample application.
