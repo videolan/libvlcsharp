8d5d6b864eca046352f654acac1c47614c5e9afa
Martin Finkel <martin@videolabs.io>
2019-06-13 11:15:35 +0700

    Add files to solution

---
+++ LibVLCSharp.Forms/Shared/IPowerManager.cs
@@ -0,0 +1,13 @@
+﻿namespace LibVLCSharp.Forms.Shared
+{
+    /// <summary>
+    /// Interface for power management.
+    /// </summary>
+    public interface IPowerManager
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the screen should be kept on.
+        /// </summary>
+        bool KeepScreenOn { get; set; }
+    }
+}
