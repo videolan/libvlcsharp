31ee2d13ab8609d4fb804ba9386f0a79240f58c7
Martin Finkel <martin@videolabs.io>
2019-06-13 11:15:35 +0700

    Add files to solution

---
+++ Samples/Forms/LibVLCSharp.Forms.MediaElement/LibVLCSharp.Forms.MediaElement.Sample/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using LibVLCSharp.Forms.Shared;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace LibVLCSharp.Forms.Sample.MediaPlayerElement
+{
+    /// <summary>
+    /// Represents the sample application.
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="App"/> class.
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        /// <summary>
+        /// Called when the application starts.
+        /// </summary>
+        protected override void OnStart()
+        {
+            base.OnStart();
+            MessagingCenter.Send(new LifecycleMessage(), nameof(OnStart));
+        }
+
+        /// <summary>
+        /// Called when the application enters the sleeping state.
+        /// </summary>
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+            MessagingCenter.Send(new LifecycleMessage(), nameof(OnSleep));
+        }
+
+        /// <summary>
+        /// Called when the application resumes from the sleeping state.
+        /// </summary>
+        protected override void OnResume()
+        {
+            base.OnResume();
+            MessagingCenter.Send(new LifecycleMessage(), nameof(OnResume));
+        }
+    }
+}
