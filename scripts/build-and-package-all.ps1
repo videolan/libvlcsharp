fdcc3cf0113d378a397d66c6437eea07e3f95dbd
Martin Finkel <finkel2804@gmail.com>
2019-08-26 18:29:53 +0200

    packaging: Add package count check

--- scripts/build-and-package-all.ps1
+++ scripts/build-and-package-all.ps1
@@ -1,3 +1,7 @@
+del *.nupkg
+
+$libvlcsharpPackagesCount = 7;
+
 .\build-and-package-libvlcsharp.ps1
 .\build-and-package-libvlcsharp-forms.ps1
 .\build-and-package-libvlcsharp-wpf.ps1
@@ -5,3 +9,14 @@
 .\build-and-package-libvlcsharp-gtk.ps1
 .\build-and-package-libvlcsharp-forms-gtk.ps1
 .\build-and-package-libvlcsharp-winforms.ps1
+
+$nugetPackages = Get-ChildItem -recurse -filter *.nupkg | Group-Object -Property Directory
+
+if($nugetPackages.Count -eq $libvlcsharpPackagesCount)
+{
+    Write-Output "All $($libvlcsharpPackagesCount) packages built successfully"
+}
+else
+{
+    Write-Error "Not all $($libvlcsharpPackagesCount) packages built successfully"
+}
