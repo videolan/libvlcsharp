e23293a35eb09c8904338447a0ee822fbb89aa24
Martin Finkel <finkel2804@gmail.com>
2019-09-16 17:59:20 +0200

    WPF: TreatWarningsAsErrors is now enabled

--- LibVLCSharp.WPF/VideoView.cs
+++ LibVLCSharp.WPF/VideoView.cs
@@ -8,6 +8,9 @@ using System.Windows.Forms.Integration;
 
 namespace LibVLCSharp.WPF
 {
+    /// <summary>
+    /// WPF VideoView with databinding for use with LibVLCSharp
+    /// </summary>
     [TemplatePart(Name = PART_PlayerHost, Type = typeof(WindowsFormsHost))]
     [TemplatePart(Name = PART_PlayerView, Type = typeof(System.Windows.Forms.Panel))]
     public class VideoView : ContentControl, IVideoView, IDisposable
@@ -15,16 +18,25 @@ namespace LibVLCSharp.WPF
         private const string PART_PlayerHost = "PART_PlayerHost";
         private const string PART_PlayerView = "PART_PlayerView";
 
+        /// <summary>
+        /// WPF VideoView constructor
+        /// </summary>
         public VideoView()
         {
             DefaultStyleKey = typeof(VideoView);
         }
 
+        /// <summary>
+        /// MediaPlayer WPF databinding property
+        /// </summary>
         public static readonly DependencyProperty MediaPlayerProperty = DependencyProperty.Register(nameof(MediaPlayer),
                 typeof(MediaPlayer),
                 typeof(VideoView),
                 new PropertyMetadata(null, OnMediaPlayerChanged));
 
+        /// <summary>
+        /// MediaPlayer property for this VideoView
+        /// </summary>
         public MediaPlayer MediaPlayer
         {
             get { return GetValue(MediaPlayerProperty) as MediaPlayer; }
@@ -49,6 +61,9 @@ namespace LibVLCSharp.WPF
         private UIElement ViewContent { get; set; }
         private IntPtr Hwnd { get; set; }
 
+        /// <summary>
+        /// ForegroundWindow management and MediaPlayer setup.
+        /// </summary>
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
@@ -81,6 +96,11 @@ namespace LibVLCSharp.WPF
             }
         }
 
+        /// <summary>
+        /// Override to update the foreground window content
+        /// </summary>
+        /// <param name="oldContent">old content</param>
+        /// <param name="newContent">new content</param>
         protected override void OnContentChanged(object oldContent, object newContent)
         {
             base.OnContentChanged(oldContent, newContent);
@@ -110,6 +130,10 @@ namespace LibVLCSharp.WPF
         #region IDisposable Support
 
         bool disposedValue;
+        /// <summary>
+        /// Unhook mediaplayer and dispose foreground window
+        /// </summary>
+        /// <param name="disposing"></param>
         protected virtual void Dispose(bool disposing)
         {
             if (!disposedValue)
@@ -129,6 +153,9 @@ namespace LibVLCSharp.WPF
             }
         }
 
+        /// <summary>
+        /// Unhook mediaplayer and dispose foreground window
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
