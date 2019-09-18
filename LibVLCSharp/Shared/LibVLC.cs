77fcd213d6e32da81117840463180912d338c46c
Martin Finkel <finkel2804@gmail.com>
2019-09-18 17:54:50 +0200

    Core logging: Add another defensive null check
    When libvlc gets disposed we unsubscribe, but a previous log callback might be raised right after or while libvlc gets disposed and just before the event handler is invoked.

--- LibVLCSharp/Shared/LibVLC.cs
+++ LibVLCSharp/Shared/LibVLC.cs
@@ -643,9 +643,9 @@ namespace LibVLCSharp.Shared
 
                 GetLogContext(ctx, out var module, out var file, out var line);
 #if NET40
-                Task.Factory.StartNew(() => libvlc._log?.Invoke(null, new LogEventArgs(level, message, module, file, line)));
+                Task.Factory.StartNew(() => libvlc?._log?.Invoke(null, new LogEventArgs(level, message, module, file, line)));
 #else
-                Task.Run(() => libvlc._log?.Invoke(null, new LogEventArgs(level, message, module, file, line)));
+                Task.Run(() => libvlc?._log?.Invoke(null, new LogEventArgs(level, message, module, file, line)));
 #endif
             }
             catch
