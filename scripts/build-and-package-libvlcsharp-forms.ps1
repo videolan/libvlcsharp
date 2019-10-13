1bb5b8eea4b49fcb3092601f272bf75c993b91ce
Martin Finkel <martin@videolabs.io>
2019-10-13 17:02:22 +0700

    CI: Build from powershell script, in release mode
    The TF_BUILD env variable is used to determine whether we're running on CI or
    not.
    
    Azure DevOps uses the Enterprise VS version while when execute locally,
    the script looks for the Community VS version.
    
    The CI will fail if no exit code is explicitly returned. On the other hand, the
    terminal will exit when returning an exit code locally. So only return when
    running on CI.

--- scripts/build-and-package-libvlcsharp-forms.ps1
+++ scripts/build-and-package-libvlcsharp-forms.ps1
@@ -1,7 +1,13 @@
-Remove-Item ..\LibVLCSharp.Forms\bin -Force -Recurse
-Remove-Item ..\LibVLCSharp.Forms\obj -Force -Recurse
+param([string]$msbuild)
 
-$msbuild =  '"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"'
+Remove-Item ..\LibVLCSharp.Forms\bin -Force -Recurse -ErrorAction Ignore
+Remove-Item ..\LibVLCSharp.Forms\obj -Force -Recurse -ErrorAction Ignore
+
+if(!$msbuild)
+{
+    $msbuild = "'C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe'";
+}
+
 iex ("& {0} {1}" -f $msbuild, "../LibVLCSharp.Forms/LibVLCSharp.Forms.csproj /t:Restore,Clean,Build /p:Configuration=Release")
 
 robocopy  ..\LibVLCSharp.Forms\bin\Release\ . *.nupkg
