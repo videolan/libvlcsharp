700f98d975f7ed8d4272b302f82ce2bcb899c598
Martin Finkel <martin@videolabs.io>
2019-10-13 16:32:31 +0700

    Use newer msbuild

--- scripts/build-and-package-libvlcsharp-gtk.ps1
+++ scripts/build-and-package-libvlcsharp-gtk.ps1
@@ -1,7 +1,7 @@
 Remove-Item ..\LibVLCSharp.GTK\bin -Force -Recurse
 Remove-Item ..\LibVLCSharp.GTK\obj -Force -Recurse
 
-$msbuild =  '"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"'
+$msbuild =  '"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"'
 iex ("& {0} {1}" -f $msbuild, "../LibVLCSharp.GTK/LibVLCSharp.GTK.csproj /t:Restore,Clean,Build /p:Configuration=Release")
 
 robocopy  ..\LibVLCSharp.GTK\bin\Release\ . *.nupkg
--- scripts/build-and-package-libvlcsharp-winforms.ps1
+++ scripts/build-and-package-libvlcsharp-winforms.ps1
@@ -1,7 +1,7 @@
 Remove-Item ..\LibVLCSharp.WinForms\bin -Force -Recurse
 Remove-Item ..\LibVLCSharp.WinForms\obj -Force -Recurse
 
-$msbuild =  '"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"'
+$msbuild =  '"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"'
 iex ("& {0} {1}" -f $msbuild, "../LibVLCSharp.WinForms/LibVLCSharp.WinForms.csproj /t:Restore,Clean,Build /p:Configuration=Release")
 
 robocopy  ..\LibVLCSharp.WinForms\bin\Release\ . *.nupkg
