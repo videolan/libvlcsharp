60e481bc6afc34c6c9f659561d8d43dbc89cd2bb
Stéphane Mitermite <stephthebest@gmail.com>
2019-10-18 11:07:00 +0200

    Makes the UWP VideoView more MVVM friendly
    Replaces the SwapChainOptions dependency property by an Initialized event
    Restores GenerateLibraryLayout option for UWP libraries

---
+++ Samples/LibVLCSharp.UWP.Sample/MainViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using LibVLCSharp.Platforms.UWP;
+using LibVLCSharp.Shared;
+
+namespace LibVLCSharp.UWP.Sample
+{
+    /// <summary>
+    /// Main view model
+    /// </summary>
+    public class MainViewModel : INotifyPropertyChanged, IDisposable
+    {
+        /// <summary>
+        /// Occurs when a property value changes
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Initialized a new instance of <see cref="MainViewModel"/> class
+        /// </summary>
+        public MainViewModel()
+        {
+            InitializedCommand = new RelayCommand<InitializedEventArgs>(Initialize);
+        }
+
+        /// <summary>
+        /// Destructor
+        /// </summary>
+        ~MainViewModel()
+        {
+            Dispose();
+        }
+
+        /// <summary>
+        /// Gets the command for the initialization
+        /// </summary>
+        public ICommand InitializedCommand { get; }
+
+        private LibVLC LibVLC { get; set; }
+
+        private MediaPlayer _mediaPlayer;
+        /// <summary>
+        /// Gets the media player
+        /// </summary>
+        public MediaPlayer MediaPlayer
+        {
+            get => _mediaPlayer;
+            private set => Set(nameof(MediaPlayer), ref _mediaPlayer, value);
+        }
+
+        private void Set<T>(string propertyName, ref T field, T value)
+        {
+            if (field == null && value != null || field != null && !field.Equals(value))
+            {
+                field = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void Initialize(InitializedEventArgs eventArgs)
+        {
+            LibVLC = new LibVLC(eventArgs.SwapChainOptions);
+            MediaPlayer = new MediaPlayer(LibVLC);
+            MediaPlayer.Play(new Media(LibVLC, "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                FromType.FromLocation));
+        }
+
+        /// <summary>
+        /// Cleaning
+        /// </summary>
+        public void Dispose()
+        {
+            var mediaPlayer = MediaPlayer;
+            MediaPlayer = null;
+            mediaPlayer?.Dispose();
+            LibVLC?.Dispose();
+            LibVLC = null;
+        }
+    }
+}
