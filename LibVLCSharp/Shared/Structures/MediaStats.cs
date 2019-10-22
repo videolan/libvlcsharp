030f2048f40264b3d844e2378f55ed8c21a3701c
Martin Finkel <martin@videolabs.io>
2019-10-22 16:39:25 +0700

    Doc: Add info about MediaStats

--- LibVLCSharp/Shared/Structures/MediaStats.cs
+++ LibVLCSharp/Shared/Structures/MediaStats.cs
@@ -8,31 +8,79 @@ namespace LibVLCSharp.Shared
     [StructLayout(LayoutKind.Sequential)]
     public readonly struct MediaStats
     {
-        /* Input */
+        /// <summary>
+        /// The number of bytes read
+        /// </summary>
         public readonly int ReadBytes;
+
+        /// <summary>
+        /// The input bitrate
+        /// </summary>
         public readonly float InputBitrate;
 
-        /* Demux */
+        /// <summary>
+        /// The number of bytes read by the demux
+        /// </summary>
         public readonly int DemuxReadBytes;
+
+        /// <summary>
+        /// The demux bitrate
+        /// </summary>
         public readonly float DemuxBitrate;
+
+        /// <summary>
+        /// The number of frame discarded
+        /// </summary>
         public readonly int DemuxCorrupted;
+
+        /// <summary>
+        /// The number of frame dropped
+        /// </summary>
         public readonly int DemuxDiscontinuity;
 
-        /* Decoders */
+        /// <summary>
+        /// The number of decoded video blocks
+        /// </summary>
         public readonly int DecodedVideo;
+
+        /// <summary>
+        /// The number of decoded audio blocks
+        /// </summary>
         public readonly int DecodedAudio;
 
-        /* Video Output */
+        /// <summary>
+        /// The number of frames displayed
+        /// </summary>
         public readonly int DisplayedPictures;
+
+        /// <summary>
+        /// The number of frames lost
+        /// </summary>
         public readonly int LostPictures;
 
-        /* Audio output */
+        /// <summary>
+        /// The number of buffers played
+        /// </summary>
         public readonly int PlayedAudioBuffers;
+
+        /// <summary>
+        /// The number of buffers lost
+        /// </summary>
         public readonly int LostAudioBuffers;
 
-        /* Stream output */
+        /// <summary>
+        /// The number of packets sent
+        /// </summary>
         public readonly int SentPackets;
+
+        /// <summary>
+        /// The number of bytes sent
+        /// </summary>
         public readonly int SentBytes;
+
+        /// <summary>
+        /// The bitrate used to send
+        /// </summary>
         public readonly float SendBitrate;
     }
 }
