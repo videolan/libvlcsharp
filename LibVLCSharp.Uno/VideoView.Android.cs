93443db6ee0fa091738330ce077a045860d0bc91
Stéphane Mitermite <stephthebest@gmail.com>
2019-10-24 10:45:53 +0200

    Adds VideoView for Uno Platform

---
+++ LibVLCSharp.Uno/VideoView.Android.cs
@@ -0,0 +1,21 @@
+﻿using Uno.UI;
+
+namespace LibVLCSharp.Platforms.UWP
+{
+    /// <summary>
+    /// Video view
+    /// </summary>
+    public partial class VideoView
+    {
+        private Android.VideoView? UnderlyingVideoView
+        {
+            get;
+            set;
+        }
+
+        private Android.VideoView CreateVideoView()
+        {
+            return new Android.VideoView(ContextHelper.Current);
+        }
+    }
+}
---
+++ LibVLCSharp.Uno/VideoView.iOS.cs
@@ -0,0 +1,19 @@
+﻿namespace LibVLCSharp.Platforms.UWP
+{
+    /// <summary>
+    /// Video view
+    /// </summary>
+    public partial class VideoView
+    {
+        private iOS.VideoView? UnderlyingVideoView
+        {
+            get;
+            set;
+        }
+
+        private iOS.VideoView CreateVideoView()
+        {
+            return new iOS.VideoView();
+        }
+    }
+}
