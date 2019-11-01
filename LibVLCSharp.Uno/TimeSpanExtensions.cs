373fb09095ddf7c6794ae40ace69331102a92347
Stéphane Mitermite <stephthebest@gmail.com>
2019-11-01 15:09:02 +0100

    Manages the seek bar

---
+++ LibVLCSharp.Uno/TimeSpanExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LibVLCSharp.Uno
+{
+    /// <summary>
+    /// Extensions methods for <see cref="TimeSpan"/>.
+    /// </summary>
+    public static class TimeSpanExtensions
+    {
+        /// <summary>
+        /// Converts the value of the current <see cref="TimeSpan"/> object to its equivalent short string representation.
+        /// </summary>
+        /// <param name="span">Time interval.</param>
+        /// <returns>The short string representation of the current <see cref="TimeSpan"/> value.</returns>
+        internal static string ToShortString(this TimeSpan span)
+        {
+            if (span.Days != 0)
+            {
+                return span.ToString(@"d\.hh\:mm\:ss");
+            }
+            if (span.Hours != 0)
+            {
+                return span.ToString(@"hh\:mm\:ss");
+            }
+            return span.ToString(@"mm\:ss");
+        }
+    }
+}
