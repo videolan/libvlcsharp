25355fbbae70bce78fd96cd5627dc7546d0857b2
Stéphane Mitermite <stephthebest@gmail.com>
2019-11-11 23:38:34 +0100

    Adds AudioTracksManager & SubtitlesTracksManager

---
+++ LibVLCSharp/Shared/MediaPlayerElement/AudioTracksManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using LibVLCSharp.Shared.Structures;
+
+namespace LibVLCSharp.Shared.MediaPlayerElement
+{
+    /// <summary>
+    /// Audio tracks manager
+    /// </summary>
+    public class AudioTracksManager : TracksManager
+    {
+        /// <summary>
+        /// Initialized a new instance of <see cref="AudioTracksManager"/> class
+        /// </summary>
+        /// <param name="dispatcher">dispatcher</param>
+        public AudioTracksManager(IDispatcher dispatcher) : base(dispatcher, TrackType.Audio)
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the current track identifier
+        /// </summary>
+        /// <remarks>returns -1 if no active input</remarks>
+        public override int CurrentTrackId
+        {
+            get => GetCurrentTrackId(MediaPlayer?.AudioTrack);
+            set => SetCurrentTrackId(mp => mp.SetAudioTrack(value));
+        }
+
+        /// <summary>
+        /// Gets the tracks descriptions
+        /// </summary>
+        public override IEnumerable<TrackDescription> Tracks => MediaPlayer?.AudioTrackDescription;
+    }
+}
