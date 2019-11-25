videolan/libvlcsharp

commit ffba03e7cd9d8f8ac32077592681be1322e91d7d

Review feedback: Add comment for try/catch and rename variable

LibVLCSharp/Shared/Helpers/MarshalUtils.cs
@@ -120,30 +120,30 @@ namespace LibVLCSharp.Shared.Helpers
 
         static string UseStructurePointer<T>(T structure, Func<IntPtr, string> action)
         {
-            var listPointer = IntPtr.Zero;
+            var structurePointer = IntPtr.Zero;
             try
             {
-                listPointer = Marshal.AllocHGlobal(Marshal.SizeOf(structure));
-                Marshal.StructureToPtr(structure, listPointer, false);
-                return action(listPointer);
+                structurePointer = Marshal.AllocHGlobal(Marshal.SizeOf(structure));
+                Marshal.StructureToPtr(structure, structurePointer, false);
+                return action(structurePointer);
             }
             finally
             {
-                Marshal.FreeHGlobal(listPointer);
+                Marshal.FreeHGlobal(structurePointer);
             }
         }
 
         static void UseStructurePointer<T>(T structure, Action<IntPtr> action)
         {
-            var listPointer = Marshal.AllocHGlobal(Marshal.SizeOf(structure));
+            var structurePointer = Marshal.AllocHGlobal(Marshal.SizeOf(structure));
             try
             {
-                Marshal.StructureToPtr(structure, listPointer, false);
-                action(listPointer);
+                Marshal.StructureToPtr(structure, structurePointer, false);
+                action(structurePointer);
             }
             finally
             {
-                Marshal.FreeHGlobal(listPointer);
+                Marshal.FreeHGlobal(structurePointer);
             }
         }
 

LibVLCSharp/Shared/LibVLC.cs
@@ -648,6 +648,7 @@ namespace LibVLCSharp.Shared
                 Task.Run(() => libvlc._log?.Invoke(null, new LogEventArgs(level, message, module, file, line)));
 #endif
             }
+            // Silently catching OOM exceptions and others as this is not critical if it fails
             catch
             {
             }

commit 6548efe4e08dbeeea6c0b0debf8a3bcf579458eb

Logging: Fix libvlc logging for Apple platforms
Regression introduced in https://github.com/videolan/libvlcsharp/pull/66

LibVLCSharp/Shared/Helpers/MarshalUtils.cs
@@ -45,17 +45,14 @@ namespace LibVLCSharp.Shared.Helpers
 
             const string Write = "w";
 
-            [DllImport(Constants.libSystem, EntryPoint = "vsprintf", CallingConvention = CallingConvention.Cdecl)]
-            public static extern int vsprintf_apple(IntPtr buffer, IntPtr format, IntPtr args);
+            [DllImport(Constants.libSystem, EntryPoint = "vasprintf", CallingConvention = CallingConvention.Cdecl)]
+            public static extern int vasprintf_apple(ref IntPtr buffer, IntPtr format, IntPtr args);
 
             [DllImport(Constants.Libc, EntryPoint = "vsprintf", CallingConvention = CallingConvention.Cdecl)]
             public static extern int vsprintf_linux(IntPtr buffer, IntPtr format, IntPtr args);
 
             [DllImport(Constants.Msvcrt, EntryPoint = "vsprintf", CallingConvention = CallingConvention.Cdecl)]
             public static extern int vsprintf_windows(IntPtr buffer, IntPtr format, IntPtr args);
-
-            [DllImport(Constants.libSystem, EntryPoint = "vsnprintf", CallingConvention = CallingConvention.Cdecl)]
-            public static extern int vsnprintf_apple(IntPtr buffer, UIntPtr size, IntPtr format, IntPtr args);
 
             [DllImport(Constants.Libc, EntryPoint = "vsnprintf", CallingConvention = CallingConvention.Cdecl)]
             public static extern int vsnprintf_linux(IntPtr buffer, UIntPtr size, IntPtr format, IntPtr args);
@@ -68,10 +65,13 @@ namespace LibVLCSharp.Shared.Helpers
 
         internal static string GetLogMessage(IntPtr format, IntPtr args)
         {
+#if APPLE
+            return AppleLogCallback(format, args);
+#else
             // special marshalling is needed on Linux desktop 64 bits.
             if (PlatformHelper.IsLinuxDesktop && PlatformHelper.IsX64BitProcess)
             {
-                return LinuxX64Callback(format, args);
+                return LinuxX64LogCallback(format, args);
             }
 
             var byteLength = vsnprintf(IntPtr.Zero, UIntPtr.Zero, format, args) + 1;
@@ -89,9 +89,28 @@ namespace LibVLCSharp.Shared.Helpers
             {
                 Marshal.FreeHGlobal(buffer);
             }
+#endif
         }
 
-        static string LinuxX64Callback(IntPtr format, IntPtr args)
+        static string AppleLogCallback(IntPtr format, IntPtr args)
+        {
+            var buffer = IntPtr.Zero;
+            try
+            {
+                var count = Native.vasprintf_apple(ref buffer, format, args);
+
+                if (count == -1)
+                    return string.Empty;
+
+                return buffer.FromUtf8();
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(buffer);
+            }
+        }
+
+        static string LinuxX64LogCallback(IntPtr format, IntPtr args)
         {
             // The args pointer cannot be reused between two calls. We need to make a copy of the underlying structure.
             var listStructure = PtrToStructure<VaListLinuxX64>(args);
@@ -151,13 +170,9 @@ namespace LibVLCSharp.Shared.Helpers
         {
 #if ANDROID
             return Native.vsnprintf_linux(buffer, size, format, args);
-#elif APPLE
-            return Native.vsnprintf_apple(buffer, size, format, args);
 #else
             if (PlatformHelper.IsWindows)
                 return Native.vsnprintf_windows(buffer, size, format, args);
-            else if (PlatformHelper.IsMac)
-                return Native.vsnprintf_apple(buffer, size, format, args);
             else if (PlatformHelper.IsLinux)
                 return Native.vsnprintf_linux(buffer, size, format, args);
             return -1;
@@ -168,20 +183,16 @@ namespace LibVLCSharp.Shared.Helpers
         {
 #if ANDROID
             return Native.vsprintf_linux(buffer, format, args);
-#elif APPLE
-            return Native.vsprintf_apple(buffer, format, args);
 #else
             if (PlatformHelper.IsWindows)
                 return Native.vsprintf_windows(buffer, format, args);
-            else if (PlatformHelper.IsMac)
-                return Native.vsprintf_apple(buffer, format, args);
             else if (PlatformHelper.IsLinux)
                 return Native.vsprintf_linux(buffer, format, args);
             return -1;
 #endif
         }
         
-        #endregion
+#endregion
 
         /// <summary>
         /// Helper for libvlc_new