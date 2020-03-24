bbef077d2a8cd50e049a4192e217b739f5981014
Martin Finkel <finkel2804@gmail.com>
2020-03-24 15:15:37 +0100

    Samples: Update smaller samples to C# 8

--- Samples/LibVLCSharp.Windows.Net40.Sample/Program.cs
+++ Samples/LibVLCSharp.Windows.Net40.Sample/Program.cs
@@ -9,10 +9,11 @@ namespace LibVLCSharp.Windows.Net40.Sample
         {
             Core.Initialize();
 
-            var libVLC = new LibVLC();
-            var media = new Media(libVLC, "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4", FromType.FromLocation);
-            var mp = new MediaPlayer(media);
+            using var libVLC = new LibVLC();
+            using var media = new Media(libVLC, "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4", FromType.FromLocation);
+            using var mp = new MediaPlayer(media);
             mp.Play();
+
             Console.ReadKey();
         }
     }
