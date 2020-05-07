c27cede01a56653f9025ceec68008eae0876f3a2
Martin Finkel <finkel2804@gmail.com>
2020-05-07 11:24:38 +0200

    Update Resource.Designer.cs files
    autogen by VS every time

--- samples/Forms/LibVLCSharp.Forms.Sample.Android/Resources/Resource.designer.cs
+++ samples/Forms/LibVLCSharp.Forms.Sample.Android/Resources/Resource.designer.cs
@@ -15,7 +15,7 @@ namespace LibVLCSharp.Forms.Sample.Droid
 {
 	
 	
-	[System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
 	public partial class Resource
 	{
 		
