e5426f12bfb5bf98f4c0da529a78cfdf5fc0f3d3
afunc233 <afunc@sina.com>
2021-12-06 00:01:37 +0800

    A way to support ScrollViewer controls and Modify Sample

--- src/LibVLCSharp.WPF/ForegroundWindow.cs
+++ src/LibVLCSharp.WPF/ForegroundWindow.cs
@@ -3,13 +3,76 @@ using System;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
+
+using System.Runtime.InteropServices;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Interop;
 
 namespace LibVLCSharp.WPF
 {
     internal class ForegroundWindow : Window
     {
+        [DllImport("User32.dll", SetLastError = true)]
+        static extern int SetWindowRgn(IntPtr hWnd, IntPtr hRgn, bool bRedraw);
+
+        [DllImport("gdi32.dll")]
+        static extern IntPtr CreateRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect);
+
+        public void SetWindowRgn(Window window, Rect rect)
+        {
+            var windowHandle = new WindowInteropHelper(window).Handle;
+            SetWindowRgn(windowHandle, CreateRectRgn((int)rect.Left, (int)rect.Top, (int)rect.Right, (int)rect.Bottom), true);
+        }
+
+        private void RefrashRgn()
+        {
+            if (_wndhost == null)
+                return;
+
+            var locationFromScreen = _wndhost.PointToScreen(_zeroPoint);
+            if (locationFromScreen == null)
+                return;
+            var source = PresentationSource.FromVisual(_wndhost);
+            var targetPoints = source.CompositionTarget.TransformFromDevice.Transform(locationFromScreen);
+            if (!(PresentationSource.FromVisual(_wndhost).RootVisual is FrameworkElement rootVisual))
+                return;
+
+            var rectMain = new Rect(targetPoints.X, targetPoints.Y, rootVisual.ActualWidth,
+                rootVisual.ActualHeight - (_wndhost.WindowStyle == WindowStyle.None ? SystemParameters.CaptionHeight : SystemParameters.MinimumWindowHeight));
+
+            var win1Rect = new Rect(Left, Top, ActualWidth, ActualHeight);
+
+            var interscet = Rect.Intersect(win1Rect, rectMain);
+            if (interscet.IsEmpty)
+            {
+                Hide();
+            }
+            else
+            {
+                var rect = Rect.Empty;
+                if (win1Rect.X < interscet.X && win1Rect.Y == interscet.Y)
+                {
+                    rect = new Rect(interscet.X - win1Rect.X, 0, interscet.Width, interscet.Height);
+                }
+                if (win1Rect.X < interscet.X && win1Rect.Y < interscet.Y)
+                {
+                    rect = new Rect(interscet.X - win1Rect.X, interscet.Y - win1Rect.Y, interscet.Width, Height);
+                }
+                if (win1Rect.X == interscet.X && win1Rect.Y == interscet.Y)
+                {
+                    rect = new Rect(0, 0, interscet.Width, interscet.Height);
+                }
+                if (win1Rect.X == interscet.X && win1Rect.Y < interscet.Y)
+                {
+                    rect = new Rect(0, interscet.Y - win1Rect.Y, interscet.Width, interscet.Height);
+                }
+                SetWindowRgn(this, rect);
+                Show();
+            }
+        }
+
+
         Window? _wndhost;
         readonly FrameworkElement _bckgnd;
         readonly Point _zeroPoint = new Point(0, 0);
@@ -121,6 +184,7 @@ namespace LibVLCSharp.WPF
             var targetPoints = source.CompositionTarget.TransformFromDevice.Transform(locationFromScreen);
             Left = targetPoints.X;
             Top = targetPoints.Y;
+            RefrashRgn();
         }
 
         void Wndhost_SizeChanged(object sender, SizeChangedEventArgs e)
@@ -138,6 +202,7 @@ namespace LibVLCSharp.WPF
             var size = new Point(_bckgnd.ActualWidth, _bckgnd.ActualHeight);
             Height = size.Y;
             Width = size.X;
+            RefrashRgn();
         }
 
         void Wndhost_Closing(object sender, System.ComponentModel.CancelEventArgs e)
