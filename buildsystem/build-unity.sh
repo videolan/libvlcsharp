d3db0d64f4cf9a3e50e12f1b309c05e77e109283
Alexandre Janniaux <ajanni@videolabs.io>
2023-05-09 17:26:23 +0200

    buildsystem: build-unity.sh: setup SCRIPT_DIR
    Setup a SCRIPT_DIR bash variable to locate where the
    buildsystem/build-unity.sh script is, so as to locate the LibVLCSharp
    project reliably.

--- buildsystem/build-unity.sh
+++ buildsystem/build-unity.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-dotnet build ../LibVLCSharp/LibVLCSharp.csproj /p:UNITY=true -c Release
+
+SCRIPT_DIR="$(cd $(dirname "$0"); pwd)"
+dotnet build "${SCRIPT_DIR}/../src/LibVLCSharp/LibVLCSharp.csproj" /p:UNITY=true -c Release
