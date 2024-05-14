b1d34447a188b6fa30d3ae769848210f37b42e8a
Martin Finkel <me@martinfinkel.com>
2024-05-14 19:47:48 +0700

    MAUI: add initial integration

---
+++ src/LibVLCSharp.MAUI/Handlers/VideoViewHandler.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Maui.Handlers;
+#if ANDROID
+using VideoViewImpl = LibVLCSharp.Platforms.Android.VideoView;
+#elif IOS
+using VideoViewImpl = LibVLCSharp.Platforms.iOS.VideoView;
+#endif 
+
+namespace LibVLCSharp.MAUI
+{
+    /// <summary>
+    /// MAUI view handler for the VideoView
+    /// </summary>
+    public partial class VideoViewHandler : ViewHandler<VideoView, VideoViewImpl>
+    {
+        /// <inheritdoc />
+        protected override void ConnectHandler(VideoViewImpl platformView) => base.ConnectHandler(platformView);
+
+        /// <inheritdoc />
+        protected override void DisconnectHandler(VideoViewImpl platformView) => base.DisconnectHandler(platformView);
+
+        /// <inheritdoc />
+        protected override VideoViewImpl CreatePlatformView()
+        {
+#if ANDROID
+            return new VideoViewImpl(Context);
+#else
+            return new VideoViewImpl();
+#endif
+        }
+    }
+}
---
+++ src/LibVLCSharp.MAUI/VideoView.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using LibVLCSharp.Shared;
+
+namespace LibVLCSharp.MAUI
+{
+    /// <summary>
+    /// Generic MAUI VideoView
+    /// </summary>
+    public class VideoView : View//, IVideoControl
+    {
+        /// <summary>
+        /// Raised when a new MediaPlayer is set and will be attached to the view
+        /// </summary>
+        public event EventHandler<MediaPlayerChangingEventArgs>? MediaPlayerChanging;
+
+        /// <summary>
+        /// Raised when a new MediaPlayer is set and attached to the view
+        /// </summary>
+        public event EventHandler<MediaPlayerChangedEventArgs>? MediaPlayerChanged;
+
+        /// <summary>
+        /// Xamarin.Forms MediaPlayer databinded property
+        /// </summary>
+        public static readonly BindableProperty MediaPlayerProperty = BindableProperty.Create(nameof(MediaPlayer),
+                typeof(LibVLCSharp.Shared.MediaPlayer),
+                typeof(VideoView),
+                propertyChanging: OnMediaPlayerChanging,
+                propertyChanged: OnMediaPlayerChanged);
+
+        /// <summary>
+        /// The MediaPlayer object attached to this view
+        /// </summary>
+        public LibVLCSharp.Shared.MediaPlayer? MediaPlayer
+        {
+            get { return GetValue(MediaPlayerProperty) as LibVLCSharp.Shared.MediaPlayer; }
+            set { SetValue(MediaPlayerProperty, value); }
+        }
+
+        private static void OnMediaPlayerChanging(BindableObject bindable, object oldValue, object newValue)
+        {
+            var videoView = (VideoView)bindable;
+            Debug.WriteLine("OnMediaPlayerChanging");
+            videoView.MediaPlayerChanging?.Invoke(videoView, new MediaPlayerChangingEventArgs(oldValue as LibVLCSharp.Shared.MediaPlayer, newValue as LibVLCSharp.Shared.MediaPlayer));
+        }
+
+        private static void OnMediaPlayerChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var videoView = (VideoView)bindable;
+            Debug.WriteLine("OnMediaPlayerChanged");
+            videoView.MediaPlayerChanged?.Invoke(videoView, new MediaPlayerChangedEventArgs(oldValue as LibVLCSharp.Shared.MediaPlayer, newValue as LibVLCSharp.Shared.MediaPlayer));
+        }
+    }
+}
