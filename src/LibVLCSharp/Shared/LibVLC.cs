351ca877018df12f45f581c59ff811ead8127754
Martin Finkel <me@martinfinkel.com>
2025-12-01 13:23:16 +0700

    logfile: reset log file handle
    (cherry picked from commit dfab98173606070c15036b2ed3344603f8967e14)

--- src/LibVLCSharp/Shared/LibVLC.cs
+++ src/LibVLCSharp/Shared/LibVLC.cs
@@ -496,7 +496,9 @@ namespace LibVLCSharp.Shared
         {
             if (_logFileHandle == IntPtr.Zero) return true;
 
-            return MarshalUtils.Close(_logFileHandle);
+            var result = MarshalUtils.Close(_logFileHandle);
+            _logFileHandle = IntPtr.Zero;
+            return result;
         }
 
         /// <summary>Sets up logging to a file.
