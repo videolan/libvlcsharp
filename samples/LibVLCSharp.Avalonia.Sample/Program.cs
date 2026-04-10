87ae195698f200ac25c592f199cd1fe08b0442bc
Martin Finkel <me@martinfinkel.com>
2026-04-10 17:07:09 +0700

    avalonia: Remove deprecated ReactiveUI dependency from Avalonia sample
    (cherry picked from commit bd8d6bbfde4566e0920957e0140abffcf34a8eb5)

--- samples/LibVLCSharp.Avalonia.Sample/Program.cs
+++ samples/LibVLCSharp.Avalonia.Sample/Program.cs
@@ -1,23 +1,15 @@
-﻿using Avalonia;
-using Avalonia.ReactiveUI;
+using Avalonia;
 
 namespace LibVLCSharp.Avalonia.Sample
 {
     class Program
     {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
         public static void Main(string[] args) => BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
 
-        // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
-                .With(new SkiaOptions{ MaxGpuResourceSizeBytes = 8096000})
-                .LogToTrace()
-                .UseSkia()
-                .UseReactiveUI();
+                .LogToTrace();
     }
 }
--- samples/LibVLCSharp.Avalonia.Sample/ViewModels/ViewModelBase.cs
+++ samples/LibVLCSharp.Avalonia.Sample/ViewModels/ViewModelBase.cs
@@ -1,8 +1,15 @@
-﻿using ReactiveUI;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace LibVLCSharp.Avalonia.Sample.ViewModels
 {
-    public class ViewModelBase : ReactiveObject
+    public class ViewModelBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
