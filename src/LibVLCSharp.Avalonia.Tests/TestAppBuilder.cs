ac274eaf211e7c570a75b204019bf8682663edd8
ibram-reda <37075700+ibram-reda@users.noreply.github.com>
2026-06-01 20:49:40 +0300

    test(avalonia): add failing test cases demonstrating VideoView removal issue

---
+++ src/LibVLCSharp.Avalonia.Tests/TestAppBuilder.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia;
+using Avalonia.Headless;
+
+[assembly: AvaloniaTestApplication(typeof(TestAppBuilder))]
+public class TestAppBuilder
+{
+    public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<LibVLCSharp.Avalonia.Tests.App>()
+        .UseHeadless(new AvaloniaHeadlessPlatformOptions());
+}
---
+++ src/LibVLCSharp.Avalonia.Tests/VideoViewTests.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia.Controls;
+using Avalonia.Headless.NUnit;
+using Avalonia.VisualTree;
+using NUnit.Framework;
+
+namespace LibVLCSharp.Avalonia.Tests
+{
+    public class VideoViewTests
+    {
+        [TestCase(true)]
+        [TestCase(false)]
+        [AvaloniaTest]
+        public void VideoView_Should_Be_Removed_From_VisualTree_When_Parent_Is_Removed(bool withContent)
+        {
+            // Setup controls:
+            var videoView = new VideoView
+            {
+                Name = "VideoView",
+                Content = withContent ? new TextBox() { Name = "VideoViewTextBox", Text = "" } : null
+            };
+            var secondView = new TextBox() { Name = "Second", Text = "" };
+            var window = new Window { Content = videoView };
+
+            // Open window:
+            window.Show();
+            // change Content of window:
+            window.Content = secondView;
+            // wait for the UI to process the change:
+            window.Dispatcher.RunJobs();
+
+            var RemovedVideoView = window.FindDescendantOfType<VideoView>(false, t => t.Name == "VideoView");
+            var secondTextBox = window.FindDescendantOfType<TextBox>(false, t => t.Name == "Second");
+            Assert.That(RemovedVideoView, Is.Null);
+            Assert.That(secondTextBox, Is.Not.Null);
+        }
+    }
+}
