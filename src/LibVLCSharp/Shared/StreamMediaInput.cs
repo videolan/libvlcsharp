10a30fa6206ed0f49cbfaf3c7dadc8c9ba9b59af
Martin Finkel <me@martinfinkel.com>
2026-09-21 12:58:54 -0400

    core: optimize StreamMediaInput read callback
    close https://code.videolan.org/videolan/LibVLCSharp/-/work_items/443

--- src/LibVLCSharp/Shared/StreamMediaInput.cs
+++ src/LibVLCSharp/Shared/StreamMediaInput.cs
@@ -70,11 +70,6 @@ namespace LibVLCSharp.Shared
         {
             try
             {
-                if(_stream.CanSeek)
-                {
-                    if (_stream.Position == _stream.Length)
-                        return 0;
-                }
 #if NET40
                 var read = _stream.Read(_readBuffer, 0, Math.Min((int)len, _readBuffer.Length));
                 Marshal.Copy(_readBuffer, 0, buf, read);
